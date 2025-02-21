0023a5df8d5b0177c7c5337b1f266410a18d97c8
Thamyres França <136113451+ThamyresF@users.noreply.github.com>
2025-02-21 16:51:30 +0000

    Missões 01-02 feitas!

---
+++ projeto01.py
@@ -0,0 +1,18 @@
+# Missão 1
+
+nota = float(input("Digite a nota do Aluno: "))
+
+if nota >= 6:
+    print("Aluno aprovado!!!")
+elif nota <= 5:
+    print("Aluno reprovado!!!")    
+
+
+# Missão 2
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= 16:
+    print("Você pode votar!!!")
+else:
+    print("Você ainda não pode votar!")
